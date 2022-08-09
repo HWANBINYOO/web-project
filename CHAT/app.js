ea34e821c26e4f287b0929ebfebccabf302dc8ce
hawnbin <dngh0825@naver.com>
2022-08-09 15:19:29 +0900

    채팅 기능 성공

--- CHAT/app.js
+++ CHAT/app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require("path");
 const server = http.createServer(app);
 const socketIO = require("socket.io");
+const moment = require("moment");
 
 const io = socketIO(server);
 
@@ -13,9 +14,13 @@ const PORT = process.env.PORT || 5000;
 
 io.on("connection",(socket)=>{
     socket.on("chatting",(data)=>{
-        io.emit("chatting",data)
+        const {name , msg} = data;
+        io.emit("chatting",{
+            name,
+            msg,
+            time : moment(new Date()).format("h:ss A")
+        })
     })
 }) 
-
 
 server.listen(PORT, ()=> console.log(`server is runnung ${PORT}`))
--- CHAT/src/js/chat.js
+++ CHAT/src/js/chat.js
@@ -17,9 +17,28 @@ sendButton.addEventListener("click",()=>{
 })
 
 socket.on("chatting",(data)=>{
-    const li = document.createElement("li");
-    li.innerText = `${data.name} 님이 - ${data.msg}`;
-    chatList.appendChild(li);
+    const {name,msg,time} = data
+    const item = new LiModel(name,msg,time);
+    item.makeLi();
 })
 
-console.log(socket);
+function LiModel(name,msg,time){
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = ()=>{
+        const li = document.createElement("li");
+        li.classList.add(nickname.value === this.name ? "sent" : "received")
+        const dom = `
+        <span class="profile">
+            <span class="user">${this.name}</span>
+            <img class="image" src="https://placeimg.com/50/50/any" alt="">
+        </span>
+        <span class="message">${this.msg}</span>
+        <span class="time">${this.time}</span>
+        `;
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+    }
+} 
