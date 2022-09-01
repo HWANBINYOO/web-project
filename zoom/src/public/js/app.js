f0671c68583171fc3eaae51f5b337385a9b1ab56
hawnbin <dngh0825@naver.com>
2022-09-01 16:58:04 +0900

    Camera Switch

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -13,24 +13,34 @@ async function getCameras(){
     try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
         cameras.forEach(camera => {
             const option = document.createElement("option");
             option.value = camera.deviceId;
             option.innerText = camera.label;
+            if(currentCamera.label === camera.label){
+                option.selected = true; 
+            }
             camerasSelect.appendChild(option);
         });
-        console.log(cameras);
     } catch (e) {
         console.log(e);
     }
 }  
 
-async function getMedia(){
+async function getMedia(deviceId){
+    const initialConstrains = {     // deviedId 가 없을떄 , cameras 를 만들기 전
+        audio:true , 
+        video:{facingMode:"user"},
+    };
+    const caneraCibstraubts = {
+        audio:true,
+        video:{deviceId: { exact: deviceId }} 
+    };
     try {
-        myStream = await navigator.mediaDevices.getUserMedia({  // 카메라,오디오 가져온다
-                audio:true,
-                video:true,
-        });
+        myStream = await navigator.mediaDevices.getUserMedia(    // 카메라,오디오 가져온다.
+            deviceId? caneraCibstraubts : initialConstrains
+        ); 
         myFace.srcObject = myStream;
         getCameras();
     } catch(e) {
@@ -66,5 +76,10 @@ function handleCameraClick() {
     }
 }
 
+async function handleCameraChange(){
+    await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click" , handleMutiClick);
 cameraBtn.addEventListener("click" , handleCameraClick);
+camerasSelect.addEventListener("input" , handleCameraChange);
